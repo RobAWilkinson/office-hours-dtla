d8b7389067e881565c732c0495d42bbb369561d8
Robert Wilkinson <wilkinson.robert.a@gmail.com>
2015-05-22 13:29:44 -0700

    worker on heroku

--- worker.js
+++ worker.js
@@ -1,8 +1,7 @@
 var kue = require('kue');
-var url = require('url');
-var parse = require('url-parse');
 var redis  = require('kue/node_modules/redis');
 var mongoose = require('mongoose');
+var url = require('url');
 var queue = kue.createQueue();
 mongoose.connect(process.env.MONGOLAB_URI);
 // set up our models
@@ -15,11 +14,11 @@ var reservationSchema = new mongoose.Schema({
   }
 });
 var ReservationModel = mongoose.model('Reservation', reservationSchema);
-var redisUrl = parse(process.env.REDIS_URL, true);
+var redisUrl = url.parse(process.env.REDIS_URL);
 var queue = kue.createQueue({
   redis: {
-    port: redisUrl.port,
-    host: redisUrl.host,
+    host: redisUrl.hostname,
+    port: parseInt(redisUrl.port),
     auth: 'p7ptn4ckh9k5c8daco8nmps58hn'
   }
 });
