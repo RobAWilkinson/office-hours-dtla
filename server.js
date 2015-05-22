f3a8503ca6ad23f3cde2077e96229bfee76e64f5
Robert Wilkinson <wilkinson.robert.a@gmail.com>
2015-05-22 12:36:27 -0700

    worker on heroku

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ var redis  = require('kue/node_modules/redis');
 var bodyParser = require('body-parser');
 
 // config
-var queue = kue.createQueue();
+var queue = kue.createQueue({
+    redis: process.env.REDIS_URL
+});
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(morgan('combined'));
@@ -22,7 +24,7 @@ app.locals.moment = require('moment');
 
 // connect to the db
 //
-mongoose.connect('mongodb://localhost:27017/reservations');
+mongoose.connect(process.env.MONGOLAB_URI);
 // set up our models
 // j
 var reservationSchema = new mongoose.Schema({
@@ -35,14 +37,6 @@ var reservationSchema = new mongoose.Schema({
 var ReservationModel = mongoose.model('Reservation', reservationSchema);
 
 // set up redis
-kue.redis.createClient = function() {
-    var redisUrl = url.parse(process.env.REDIS_URL)
-      , client = redis.createClient(redisUrl.port, redisUrl.hostname);
-    if (redisUrl.auth) {
-        client.auth(redisUrl.auth.split(":")[1]);
-    }
-    return client;
-};
 
 
 app.get('/', function(req, res) {
--- worker.js
+++ worker.js
@@ -3,7 +3,7 @@ var url = require('url');
 var redis  = require('kue/node_modules/redis');
 var mongoose = require('mongoose');
 var queue = kue.createQueue();
-mongoose.connect('mongodb://localhost:27017/reservations');
+mongoose.connect(process.env.MONGOLAB_URI);
 // set up our models
 // j
 var reservationSchema = new mongoose.Schema({
@@ -14,14 +14,9 @@ var reservationSchema = new mongoose.Schema({
   }
 });
 var ReservationModel = mongoose.model('Reservation', reservationSchema);
-kue.redis.createClient = function() {
-    var redisUrl = url.parse(process.env.REDIS_URL)
-      , client = redis.createClient(redisUrl.port, redisUrl.hostname);
-    if (redisUrl.auth) {
-        client.auth(redisUrl.auth.split(":")[1]);
-    }
-    return client;
-};
+var queue = kue.createQueue({
+    redis: process.env.REDIS_URL
+});
 queue.process('erase', function(job, done) {
   console.log('queue process running');
   function eraser(job) {
